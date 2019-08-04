d9fa26787c9937ceda49f26b11da78ded799f2a1
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-08-04 22:29:31 +0700

    up

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-8669657457202056',
+    google_ad_client: 'ca-pub-9416869560043161',
     enable_page_level_ads: true
   });
   `;
